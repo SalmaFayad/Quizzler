b05c87ae29eb05805bc225f8e0a50e8b89eaadc1
SalmaFayad <salmafayad123456789@gmail.com>
2021-05-31 01:45:44 +0200

    Publishing the app

---
+++ study.dart
@@ -0,0 +1,22 @@
+
+
+void main() {
+  /*for(int i=0;i<10;i++) {
+    try {
+      print("Enter Your Birth Date :");
+      var birthDate = stdin.readLineSync();
+      var age = DateTime
+          .now()
+          .year - int.parse(birthDate);
+      print("Your Age is $age");
+      break;
+    }
+    on FormatException {
+      print("Wrong Value");
+    }
+  }
+  */
+  var x=4;
+  print (x);
+
+}
